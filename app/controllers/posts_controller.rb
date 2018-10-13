756764d492237803c8bb8a687312502df85704a8
zhifeng ou <zhifengou@hotmail.com>
2018-10-13 13:59:22 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,9 @@ class PostsController < ApplicationController
 
   # add create method here
     def create
-    @post = Post.new
-    @post.title = params[:title]
-    @post.description = params[:description]
+    @post = Post.new 
+    @post.title= params[:title]
+    @post.description= params[:description]
     @post.save
     redirect_to post_path(@post)
   end
